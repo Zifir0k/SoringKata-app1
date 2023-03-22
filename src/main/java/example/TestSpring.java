b990d5c00afc53f26efb23dde7515a30aa06632f
Zifir0k <evgenizef.ak@gmail.com>
2023-03-22 19:11:29 +1000

    First lesson

---
+++ src/main/java/example/TestSpring.java
@@ -0,0 +1,13 @@
+package example;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+
+        TestBean testBean = context.getBean("TestBean", TestBean.class);
+        System.out.println(testBean.getName());
+        context.close();
+    }
+}
