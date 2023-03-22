30f87357b5b8bf28f1843e6823e6e455d2ff4d64
Zifir0k <evgenizef.ak@gmail.com>
2023-03-23 00:57:26 +1000

    Second lesson!

---
+++ src/main/java/example/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package example;
+
+public class ClassicalMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Hungarian Rhapsody";
+    }
+}
--- src/main/java/example/TestSpring.java
+++ src/main/java/example/TestSpring.java
@@ -6,8 +6,11 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 
-        TestBean testBean = context.getBean("TestBean", TestBean.class);
-        System.out.println(testBean.getName());
+        Music music = context.getBean("musicBean", Music.class);
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+
+        musicPlayer.playMusic();
+
         context.close();
     }
 }
