757539c26392831e786a96e4273e9d44a9f37c46
Zifir0k <evgenizef.ak@gmail.com>
2023-03-23 12:49:45 +1000

    third lesson

--- src/main/java/example/TestSpring.java
+++ src/main/java/example/TestSpring.java
@@ -6,9 +6,10 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 
-        Music music = context.getBean("musicBean", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+        //Music music = context.getBean("musicBean", Music.class);
+        //MusicPlayer musicPlayer = new MusicPlayer(music);
 
+        MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
         musicPlayer.playMusic();
 
         context.close();
