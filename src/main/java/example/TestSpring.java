bdc497b2d8e3d5b835d87f279cd4ffebf114c69b
Zifir0k <evgenizef.ak@gmail.com>
2023-03-24 23:11:33 +1000

    eighth lesson

--- src/main/java/example/TestSpring.java
+++ src/main/java/example/TestSpring.java
@@ -1,10 +1,14 @@
 package example;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.lang.annotation.Annotation;
+
 //test
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
 
 //        Music rockMusic =context.getBean("someRockMusic", RockMusic.class);
 //        MusicPlayer musicPlayerRock = new MusicPlayer(rockMusic);
